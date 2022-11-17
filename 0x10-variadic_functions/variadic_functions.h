3ef25cd3dd50ebb9be209479ba9cb106c4571156
Ogoobaby <ogochukwumarygloria16@gmail.com>
2022-11-17 06:12:13 -0800

    Amended

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,12 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
-int _putchar(char c);
+#include <stdarg.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-#endif /* VARIADIC_FUNCTIONS_H */
+#endif
