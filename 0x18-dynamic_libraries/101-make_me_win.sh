979196f339a25be12e1869a9c21c2b435c9a00cf
Ogoobaby <ogochukwumarygloria16@gmail.com>
2023-01-16 16:22:40 -0800

    Tasks

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
- wget -P /tmp https://github.com/Ogoobaby/alx-low_level_programming/blob/79133350c83e6cf3b164ed99d819a5aa196b06de/0x18-dynamic_libraries/101-make_me_win.so 
- export LD_PRELOAD=/tmp/win.so
+ wget -P /tmp https://github.com/Ogoobaby/alx-low_level_programming/tree/master/0x18-dynamic_libraries/iwin.so 
+ export LD_PRELOAD=/root/iwin.so
